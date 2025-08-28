12cfcbed4f32990232a465ef5ae0bd8ec6ac4799
grayhollow <grayhollow7@gmail.com>
2025-08-28 19:23:06 +0300

    gbfasf

--- script.js
+++ script.js
@@ -4,8 +4,12 @@ let ordenrum(){
 
 console.log(ordernum);
 
+function conmes(){
+
 if (ordernum = "2377") {
     alert("Your order is in progress.");
 } else {
     alert("Order not found.");
+}
+
 }
