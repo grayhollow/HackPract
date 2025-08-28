0aab6a217953640d1676c92627992cbb320fe04c
grayhollow <grayhollow7@gmail.com>
2025-08-28 19:20:30 +0300

    csadfxzc

---
+++ script.js
@@ -0,0 +1,11 @@
+let ordenrum(){
+    document.getElementById("submitpro").value
+}
+
+console.log(ordernum);
+
+if (ordernum = "2377") {
+    alert("Your order is in progress.");
+} else {
+    alert("Order not found.");
+}
